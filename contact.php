078ea044f76904ee8d6e6adf52910300976b0c8a
NAFSEER CK <nafseer.ceekey@gmail.com>
2018-11-07 18:17:05 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,520 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Developed By M Abdur Rokib Promy">
+    <meta name="author" content="cosmic">
+    <meta name="keywords" content="Bootstrap 3, Template, Theme, Responsive, Corporate, Business">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>
+      Al Farris | Contact
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+<script src="js/html5shiv.js">
+</script>
+<script src="js/respond.min.js">
+</script>
+<![endif]-->
+ 	<style>
+	body 
+	{
+      background-color: #000000;
+     	color: #daa520;
+  	}
+	.head-section
+	{
+	background-color: #000000;
+     color: #daa520;
+	}
+	.container{
+		
+     	background-color: #000000;
+     color: #daa520;
+	}
+	.row-sm-8
+	{
+	background-color: #000000;
+     	color: #daa520;
+		}
+		.col-md-12
+		{
+		background-color: #000000;
+     	color: #daa520;
+		}
+	.navbar {
+   background-color: #000000;
+     	color: #daa520;
+		}
+		 .navbar-default{
+   background-color: #000000;
+     	color: #daa520;
+		}
+		.navbar-static-top{
+
+   background-color: #000000;
+     	color: #daa520;}
+   .navbar-header
+   { 
+   background-color: #000000;
+     	color: #daa520;
+		}
+		.navbar-toggle{
+   background-color: #000000;
+     	color: #daa520;}
+		.navbar-collapse{
+   background-color: #000000;
+     	color: #daa520;}
+		.icon-bar
+	{
+	color:#daa520;
+	}
+		.nav{
+		background-color: #000000;
+     	color: #daa520;}
+		.navbar-nav{
+		background-color: #000000;
+     	color: #daa520;}
+	.collapse
+	{
+			background-color: #000000;
+
+	color:#daa520;
+	}
+	.footer
+	{
+	background-color: #000000;
+	color:#daa520;
+	}
+	
+	.footer-small
+	{
+	background-color: #grey;
+	color:#daa520;
+	}
+	.copyright
+	{
+	background-color: #000000;
+	color:#daa520;
+	}
+	.btn-primary
+	{
+	background: #daa520;
+
+	}
+	.btn-info
+	{
+	background: #daa520;
+
+	}
+	.page-footer
+	{
+	background-color: #000000;
+	color:#daa520;
+	}
+	.social-link-footer
+	{
+	background-color: #000000;
+	color:#daa520;
+	}
+	.col-lg-5
+	{
+	background-color: #000000;
+	}
+	</style>
+  </head>
+
+  <body><bg color class="body">
+   <div class="container">
+        <div class="row-sm-8">
+          <div class="col-md-12">
+    <!--header start-->
+    <header class="head-section">
+      <div class="navbar navbar-default navbar-static-top container">
+          <div class="navbar-header">
+              <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+			  <img src="img/parallax-slider/images/IMG-20160821-WA0022.jpg" width="500" />
+			  
+                    
+                 
+        </div>
+						
+          <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                  
+				  <li>
+                      <a href="index.html">Home</a>
+                  </li>
+				  <li>
+                      <a href="about.html">About Us</a>
+                  </li>
+				  
+				  <li>
+                      <a href="products.html">Products</a>
+                  </li>
+				  
+                  <li>
+                      <a href="contact.html">Contact Us</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+    </header>
+    <!--header end-->
+ 
+
+    <!--breadcrumbs start-->
+    
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4">
+            <h1>
+              Contact Us
+            </h1>
+          </div>
+          <div class="col-lg-8 col-sm-8">
+            <ol class="breadcrumb pull-right">
+              <li>
+                <a href="index.html">
+                  Home
+                </a>
+              </li>
+            
+              <li class="active">
+                Contact Us
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--breadcrumbs end-->
+
+
+
+    <!--container start-->
+    <div class="container">
+
+
+      <div class="row">
+        <div class="col-lg-5 col-sm-5 address">
+          <section class="contact-infos">
+            <h4 class="title custom-font text-black">
+              ADDRESS
+            </h4>
+            <address>
+			Al Farris
+			International Enterprise,
+			<br>
+			Karama Street,
+			Abu Dhabi
+			<br> P.O Box 31192
+			
+              
+            </address>
+          </section>
+          <section class="contact-infos">
+            <h4 class="title custom-font text-black">
+              BUSINESS HOURS
+            </h4>
+            <p>
+              Monday - Friday 8am to 4pm
+              <br>
+              Saturday - 7am to 6pm
+              <br>
+              Sunday- Closed
+              <br>
+            </p>
+          </section>
+          <section class="contact-infos">
+            <h4>
+              TELEPHONE
+            </h4>
+            <p>
+              <i class="icon-phone">
+              </i>
+              +088-01234567890
+            </p>
+
+            <p>
+              <i class="icon-phone">
+              </i>
+              +088-01234567890
+            </p>
+
+          </section>
+        </div>
+        <div class="col-lg-7 col-sm-7 address">
+          <h4 style="color:#999999">
+            Drop us a line so that we can hear from you
+          </h4>
+          <div class="contact-form">
+		
+            <form action="#" method="post" enctype="multipart/form-data" role="form">
+              <div class="form-group">
+			  <input type="hidden" name="action" value="submit">
+                <label for="name">
+                  *Name
+                </label>
+                <input type="text" placeholder="" id="name" name="name" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="email">
+                  *Email
+                </label>
+                <input type="text" placeholder="" id="email" name="email" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="phone">
+                  Phone
+                </label>
+                <input type="text" id="phone" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="phone">
+                  *Message
+                </label>
+                <textarea placeholder="" name="message" rows="5" class="form-control">
+                </textarea>
+              </div>
+              <input type="submit" class="btn btn-info" type="submit" name="ok">
+              </button>
+            
+			<?php
+
+			
+			if(isset($_POST['ok']))
+		{
+		
+			$name=@trim(stripslashes($_POST['name']));
+			$email=@trim(stripslashes($_POST['email']));
+			$message=@trim(stripslashes($_POST['message']));
+			if(($name=="")||($email=="")||($message==""))
+			{
+			echo "<script>alert('* fields are mandatory');</script>";
+			}
+			else{
+			$from="From: $name<$email>\r\nReturn-path: $email";
+			$subject=$name;
+			$success=mail('mufid.razmin@gmail.com',$subject,$message,$from);
+			if($success)
+			{
+			echo "<script>alert('thankyou ,we will contact you soon..');</script>";
+			}
+			}
+		}
+		
+			?>
+			</form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--container end-->
+
+   
+
+  
+
+    
+
+    <!--footer start-->
+    <footer class="footer">
+	
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".1s">
+            <h1 style="font-family:Century">
+              Contact Info.
+            </h1>
+            <address>
+              <p  style="font-family:Century"><i class="fa fa-home pr-10"></i><a style="color:#ffffff"  style="font-family:Century">Address: Al Farres<br/> International Enterprise<br/> P.O Box 31192</a></p>
+              <p  style="font-family:Century"><i class="fa fa-globe pr-10"></i><a style="color:#ffffff"  >Karama Street, Abu Dhabi</a> </p>
+              <p  style="font-family:Century"><i class="fa fa-mobile pr-10"></i><a style="color:#ffffff"  >Mobile : (123) 456-7890</a> </p>
+              <p  style="font-family:Century"><i class="fa fa-phone pr-10"></i><a style="color:#ffffff"  >Phone : (123) 456-7890 </a></p>
+              <p  style="font-family:Century"><i class="fa fa-envelope pr-10"></i><a style="color:#ffffff"  >Email :   </a><a href="javascript:;" style="color:#daa520" >alfarres2016@gmail.com</a></p>
+            </address>
+          </div>
+         
+          <div class="col-lg-4 col-sm-4">
+            <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
+              <h1  style="font-family:Century">
+                Our Company
+              </h1>
+              <ul class="page-footer-list">
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="about.html"  style="font-family:Century" >About Us</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="piping.html"  style="font-family:Century">Products</a>
+                </li>
+                
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="contact.php"  style="font-family:Century">Contact Us</a>
+                </li>
+               
+              </ul>
+            </div>
+          </div>
+		  <div class="col-lg-4 col-sm-4">
+            <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
+              <h1  style="font-family:Century">
+                Product Range
+				
+              </h1>
+              <ul class="page-footer-list">
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="piping.html"  style="font-family:Century">Piping</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="valve.html"  style="font-family:Century">Valves</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="mechanical.html"  style="font-family:Century">Mechanical</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="drilling.html"  style="font-family:Century">Drilling</a>
+                </li>
+				
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="chemical.html"  style="font-family:Century">Chemical</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="structural.html"  style="font-family:Century">Structural</a>
+                </li>
+                <li>
+                  <i class="fa fa-angle-right"></i>
+                  <a href="electrical.html"  style="font-family:Century">Electrical & Instrumentation</a>
+                </li>
+				 
+                
+           
+                
+              </ul>
+            </div>
+          </div>
+		 
+        </div>
+      </div>
+    </footer>
+    <!-- footer end -->
+    <!--small footer start -->
+    <footer class="footer-small">
+	<bg color class="body">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 pull-right">
+                   
+                </div>
+                <div class="col-md-4">
+                  <div class="copyright">
+                    <p  style="font-family:Century">&copy; Copyright - Al Farres International Enterprise.
+					<br/>
+					Designed by - Web D</a></p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--small footer end-->
+
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+    <script type="text/javascript" src="js/hover-dropdown.js">
+    </script>
+    <script type="text/javascript" src="assets/bxslider/jquery.bxslider.js">
+    </script>
+
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&AMP;sensor=false">
+    </script>
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js">
+    </script>
+    <script src="js/wow.min.js">
+    </script>
+    <script>
+      wow = new WOW(
+        {
+          boxClass:     'wow',      // default
+          animateClass: 'animated', // default
+          offset:       0          // default
+        }
+      )
+        wow.init();
+    </script>
+
+
+    <script>
+      $(document).ready(function() {
+        //Set the carousel options
+        $('#quote-carousel').carousel({
+          pause: true,
+          interval: 4000,
+        }
+                                     );
+      }
+                       );
+
+      //google map
+      function initialize() {
+        var myLatlng = new google.maps.LatLng(51.508742,-0.120850);
+        var mapOptions = {
+          zoom: 5,
+          center: myLatlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Contact'
+        }
+                                           );
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+    </script>
+
+  </body>
+</html>
